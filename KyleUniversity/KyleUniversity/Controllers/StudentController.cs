abe16f97631ea3ff2217ebc50900cd2703cc180d
BigK1337 <kyleahicks@yahoo.com>
2014-07-03 12:02:30 -0400

    Kyle University: Part 4
    Breeze through the CRUD section of this project.  Now onto the next
    part.

--- KyleUniversity/KyleUniversity/Controllers/StudentController.cs
+++ KyleUniversity/KyleUniversity/Controllers/StudentController.cs
@@ -49,13 +49,19 @@ namespace KyleUniversity.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include="ID,LastName,FirstMidName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Students.Add(student);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
             }
-
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system admin.");
+            }
             return View(student);
         }
 
@@ -81,21 +87,32 @@ namespace KyleUniversity.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include="ID,LastName,FirstMidName,EnrollmentDate")] Student student)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(DataException)
+            {
+                ModelState.AddModelError("", "Unable to save changes.  Try again or contact your system admin.");
             }
             return View(student);
         }
 
         // GET: /Student/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if(saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed.  Try again or contact admin.";
             }
             Student student = db.Students.Find(id);
             if (student == null)
@@ -110,9 +127,16 @@ namespace KyleUniversity.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException)
+            {
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
             return RedirectToAction("Index");
         }
 
