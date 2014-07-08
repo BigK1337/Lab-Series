e168cb52565510d90eaa2620495c7fc7b6fc6adf
BigK1337 <kyleahicks@yahoo.com>
2014-07-08 11:49:51 -0400

    Kyle University: Part 8
    Running a problem with the Courses section.

--- KyleUniversity/KyleUniversity/Models/Enrollment.cs
+++ KyleUniversity/KyleUniversity/Models/Enrollment.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace KyleUniversity.Models
 {
@@ -18,6 +15,8 @@ namespace KyleUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+
+        [DisplayFormat(NullDisplayText="No grade")]
         public Grade? Grade { get; set; }
 
         public virtual Course Course { get; set; }
